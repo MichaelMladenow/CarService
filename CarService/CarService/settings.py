62a37a2e20803de773b4dc8310fd489e096cf94d
Sekklow <sekklowz@gmail.com>
2019-10-13 21:16:28 +0300

    Added react into the frontend app

--- CarService/CarService/settings.py
+++ CarService/CarService/settings.py
@@ -37,11 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
+    'rest_framework',
+    'users',
     'cars',
     'repairs',
-    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- CarService/users/serializers.py
+++ CarService/users/serializers.py
@@ -5,7 +5,20 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        password = serializers.CharField(write_only=True)
+        fields = ['url', 'username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
