30f8f4e7d762db24b5a50c127db94e100ddf4a00
Sekklow <sekklowz@gmail.com>
2019-10-15 22:34:33 +0300

    Linked react server with django

--- start-dev-server.sh
+++ start-dev-server.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+ps -ef | grep python | awk '{print $2}' | xargs kill -9
+ps -ef | grep npm | awk '{print $2}' | xargs kill -9
+
 python CarService/manage.py runserver &
-npm start dev &
+npm run dev &
